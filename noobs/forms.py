e9f5523b378990192c81b58d43cb77f9e7720746
gopivaibhav <gopivaibhav0@gmail.com>
2021-05-26 23:08:43 +0530

    Contact us form updated

--- noobs/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Query
-
-class Queryform(ModelForm):
-    class Meta:
-        model=Query
-        fields='__all__'
--- noobs/views.py
+++ noobs/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from .forms import Queryform
-
-def home(request):
-    form=Queryform()
-    if request.method=='POST':
-        form=Queryform(request.POST)
-        if form.is_valid():
-            form.save()        
-
-    return render(request,'index.html',{'form':form})
+from .models import Query
+def home(request):      
+    if request.method=="POST":
+        Name=request.POST.get('Name', '')
+        Email=request.POST.get('Email', '')
+        Message=request.POST.get('Message', '')
+        
+        hello=Query(Name=Name,Email=Email,Message=Message)
+        hello.save()
+    return render(request,'index.html')
 
 def new(request):
     par={'tc':100,'nc':40,'rc':20,'de':10}
